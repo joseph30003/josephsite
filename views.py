e74ce3bd0301cfcbf38246ab75d70f68b471c72f
Qin <joseph.qin@philips.com>
2016-06-28 15:59:55 -0400

    prepare

--- views.py
+++ views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import Context, loader
 
 
 def index(request):
-    return HttpResponse('My first view.')
+    t = loader.get_template('index.html')
+    d = Context({'person':{'first_name':'qin'}})
+    return HttpResponse(t.render(d))
