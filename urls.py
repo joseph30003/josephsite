06e43f104f1750852e8a36986322c104e2f2a712
Qin <joseph.qin@philips.com>
2016-07-06 10:50:42 -0400

    new apt

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index/$', views.index, name='index' ),
+    url(r'^$', views.index, name='index' ),
     url(r'^trials/$', views.get_name, name='getname'),
     url(r'^trials/file/$', views.show_file, name='showfile'),
 ]
